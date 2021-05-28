dc419e5774e862b5040a695c912009822803684b
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2021-05-28 10:51:59 -0300

    fix sql function

--- index.js
+++ index.js
@@ -24,6 +24,28 @@ const router = express.Router()
 router.get('/', (req, res) => res.json({ mensagem: 'A API esta ativa' }))
 app.use('/', router)
 
+// GET todos os funcionarios
+router.get('/funcionarios', async (req, res) => {
+	executeSql(`select * from evex.Funcionario`, null, (result) =>
+		res.json(result)
+	)
+})
+
+// GET um funcionario
+// funcionario -> id do funcionario
+router.get('/funcionario', async (req, res) => {
+	const funcionario = req.body.funcionario
+	if (!funcionario) return res.sendStatus(400)
+
+	executeSql(
+		`select * from evex.Funcionario where funcionario = @funcionario`,
+		{
+			fields: [['funcionario', sql.VarChar(255), funcionario]],
+		},
+		(result) => res.json(result)
+	)
+})
+
 // POST cadastrar novo funcionario
 router.post('/funcionarios', async (req, res) => {
 	const nome = req.body.nome
@@ -31,18 +53,21 @@ router.post('/funcionarios', async (req, res) => {
 	const departamento = req.body.departamento
 	if (!nome || !apelido || !departamento) return res.sendStatus(400)
 
-	execSQLQuery(
-		`insert into evex.Funcionario values(nome = ${nome}, apelido = ${apelido}, departamento = ${departamento})`
+	executeSql(
+		'insert into evex.Funcionario values(@nome, @apelido, @departamento)',
+		{
+			fields: [
+				['nome', sql.VarChar(255), nome],
+				['apelido', sql.VarChar(255), apelido],
+				['departamento', sql.VarChar(255), departamento],
+			],
+		},
+		(result) => {
+			if (result.error) result.status = 400
+			else result.status = 200
+			return res.sendStatus(result.status)
+		}
 	)
-})
-
-// DELETE um funcionario
-router.delete('/funcionario', async (req, res) => {
-	const id = req.body.id
-	if (!id) return res.sendStatus(400)
-
-	execSQLQuery(`delete from evex.Funcionario where id = ${id}`)
-	res.sendStatus(200)
 })
 
 // PUT editar dados de um funcionario
@@ -53,36 +78,64 @@ router.put('/funcionario', async (req, res) => {
 	const departamento = req.body.departamento
 	if (!id || !nome || !apelido || !departamento) return res.sendStatus(400)
 
-	execSQLQuery(
-		`update evex.Funcionario set nome = ${nome}, apelido = ${apelido}, departamento = ${departamento} where id = ${id}`
+	executeSql(
+		'update evex.Funcionario set nome = @nome, apelido = @apelido, departamento = @departamento where id = @id',
+		{
+			fields: [
+				['id', sql.Int, id],
+				['nome', sql.VarChar(255), nome],
+				['apelido', sql.VarChar(255), apelido],
+				['departamento', sql.Int, departamento],
+			],
+		},
+		(result) => {
+			if (result.error) result.status = 400
+			else result.status = 200
+			return res.sendStatus(result.status)
+		}
 	)
-	res.sendStatus(200)
 })
 
-// GET todos os funcionarios
-router.get('/funcionarios', async (req, res) => {
-	const funcionarios = await execSQLQuery(`select * from evex.Funcionario`)
-	res.send({ res: funcionarios })
-})
+// DELETE um funcionario
+router.delete('/funcionario', async (req, res) => {
+	const id = req.body.id
+	if (!id) return res.sendStatus(400)
 
-// GET um funcionario
-// funcionario -> id do funcionario
-router.get('/funcionario', async (req, res) => {
-	const funcionario = req.body.funcionario
-	if (!funcionario) return res.sendStatus(400)
-
-	const eventos = await execSQLQuery(
-		`select * from evex.Funcionario where funcionario = ${funcionario}`
+	executeSql(
+		'delete from evex.Funcionario where id = @id',
+		{ fields: [['id', sql.Int, id]] },
+		(result) => {
+			if (result.error) result.status = 400
+			else result.status = 200
+			return res.sendStatus(result.status)
+		}
 	)
-	res.send({ res: eventos })
 })
 
 // GET todos os departamentos
 router.get('/departamentos', async (req, res) => {
-	const departamento = await execSQLQuery(`select * from evex.Departamento`)
-	if (!departamento) return res.sendStatus(400)
+	executeSql(`select * from evex.Departamento`, null, (result) =>
+		res.json(result)
+	)
+})
 
-	res.send({ res: departamento })
+// POST novo departamento
+router.post('/departamentos', async (req, res) => {
+	const nome = req.body.nome
+	if (!nome) return res.sendStatus(400)
+
+	executeSql(
+		'insert into evex.Departamento values(@nome)',
+		{
+			fields: [['nome', sql.VarChar(255), nome]],
+		},
+		(result) => {
+			if (result.error) result.status = 400
+			else result.status = 200
+			res.sendStatus(result.status)
+			return
+		}
+	)
 })
 
 // GET todos os eventos dos quais um funcionario participa
@@ -91,14 +144,16 @@ router.get('/eventos', async (req, res) => {
 	const funcionario = req.body.funcionario
 	if (!funcionario) return res.sendStatus(400)
 
-	const eventos = await execSQLQuery(
-		`select * from evex.Participantes where funcionario = ${funcionario}`
+	executeSql(
+		`select * from evex.Participantes where funcionario = @funcionario`,
+		{
+			fields: [['funcionario', sql.VarChar(255), funcionario]],
+		},
+		(result) => res.json(result)
 	)
-	res.send({ res: eventos })
 })
 
 // POST cria um novo evento
-// responsavel -> id do funcionario responsavel
 router.post('/eventos', async (req, res) => {
 	const titulo = req.body.titulo
 	const tipo = req.body.tipo
@@ -108,14 +163,26 @@ router.post('/eventos', async (req, res) => {
 	if (!titulo || !tipo || !datahora || !responsavel)
 		return res.sendStatus(400)
 
-	const eventos =
-		await execSQLQuery(`insert into evex.Participantes values(titulo = ${titulo}, tipo = ${tipo}, subtipo = ${subtipo},
-                                        datahora = ${datahora}, responsavel = ${responsavel}`)
-	res.send({ res: eventos })
+	executeSql(
+		'insert into evex.Evento values(@titulo, @tipo, @subtipo, @datahora, @responsavel)',
+		{
+			fields: [
+				['titulo', sql.VarChar(255), titulo],
+				['tipo', sql.VarChar(255), tipo],
+				['subtipo', sql.VarChar(255), subtipo],
+				['datahora', sql.DateTime, datahora],
+				['responsavel', sql.Int, responsavel],
+			],
+		},
+		(result) => {
+			if (result.error) result.status = 400
+			else result.status = 200
+			return res.sendStatus(result.status)
+		}
+	)
 })
 
-// POST cria um novo evento
-// responsavel -> id do funcionario responsavel
+// PUT edita um evento
 router.put('/eventos', async (req, res) => {
 	const id = req.body.id
 	const titulo = req.body.titulo
@@ -126,39 +193,76 @@ router.put('/eventos', async (req, res) => {
 	if (!id || !titulo || !tipo || !datahora || !responsavel)
 		return res.sendStatus(400)
 
-	const eventos =
-		await execSQLQuery(`update evex.Participantes set titulo = ${titulo}, tipo = ${tipo}, subtipo = ${subtipo},
-                                        datahora = ${datahora}, responsavel = ${responsavel} where id = ${id}`)
-	res.send({ res: eventos })
+	executeSql(
+		'update evex.Evento set titulo = @titulo, tipo = @tipo, subtipo = @subtipo, datahora = @datahora, responsavel = @responsavel where id = @id',
+		{
+			fields: [
+				['id', sql.Int, id],
+				['titulo', sql.VarChar(255), titulo],
+				['tipo', sql.VarChar(255), tipo],
+				['subtipo', sql.VarChar(255), subtipo],
+				['datahora', sql.DateTime, datahora],
+				['responsavel', sql.Int, responsavel],
+			],
+		},
+		(result) => {
+			if (result.error) result.status = 400
+			else result.status = 200
+			return res.sendStatus(result.status)
+		}
+	)
 })
 
 // GET todos os tipos de evento
 router.get('/tipos', async (req, res) => {
-	const tipos = await execSQLQuery(`select * from evex.TipoEvento`)
-	if (!tipos) return res.sendStatus(400)
-
-	res.send({ res: tipos })
+	executeSql(`select * from evex.TipoEvento`, null, (result) =>
+		res.json(result)
+	)
 })
 
 // GET todos os subtipos de evento
 router.get('/subtipos', async (req, res) => {
-	const subtipos = await execSQLQuery(`select * from evex.SubTipoEvento`)
-	if (!subtipos) return res.sendStatus(400)
-
-	res.send({ res: subtipos })
+	executeSql(`select * from evex.SubTipoEvento`, null, (result) =>
+		res.json(result)
+	)
 })
 
 // GET todos os locais de evento
 router.get('/locais', async (req, res) => {
-	const locais = await execSQLQuery(`select * from evex.Localizacoes`)
-	if (!locais) return res.sendStatus(400)
-
-	res.send({ res: locais })
+	executeSql(`select * from evex.Localizacoes`, null, (result) =>
+		res.json(result)
+	)
 })
 
 app.listen(port, () => console.log('O servidor esta ativo'))
 
-const execSQLQuery = (sqlQry) => {
-	console.log(`[sql] exec: ${sqlQry}`)
-	return global.conn.request().query(sqlQry)
+const executeSql = async (query, fields, callback) => {
+	try {
+		const request = pool.request()
+		if (fields)
+			fields.fields.forEach((field) => {
+				request.input(field[0], field[1], field[2])
+			})
+		const result = await request.query(query)
+		let resParse = []
+		result.recordset.forEach((res) => {
+			resParse.push({ ra: res.RA, nome: res.Nome, email: res.Email })
+		})
+		callback({
+			rowsAffected: result.rowsAffected[0],
+			recordset: resParse,
+		})
+	} catch (err) {
+		let error
+		try {
+			let dbError = err.originalError.info.message
+			if (dbError.includes('Violation of PRIMARY KEY'))
+				error = 'Registro já existe'
+		} catch (e) {}
+		callback({
+			rowsAffected: 0,
+			recordset: [],
+			error: error,
+		})
+	}
 }
