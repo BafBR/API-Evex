7cc80e7a4a31fd66fe3e81da5267315416cb4be5
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2021-06-01 15:15:47 -0300

    fix cors?

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 const express = require('express')
 const sql = require('mssql')
+const cors = require('cors')
 
 const config = require('./config.json')
 
@@ -15,6 +16,7 @@ const poolConnect = pool.connect()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 
 const router = express.Router()
 
